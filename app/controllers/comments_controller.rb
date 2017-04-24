51aa43f8290e55357f2d50990d1adb9be54a0fe6
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-04-24 15:57:06 +0200

    Add validations to comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,7 +4,13 @@ class CommentsController < ApplicationController
   def create
     @comment = Comment.new(comment_params)
     @comment.spot_id = params[:spot_id]
-    @comment.save
-    redirect_to spot_path(@comment.spot)
+    # @comment.save
+    # redirect_to spot_path(@comment.spot)
+    if @comment.save
+      redirect_to spot_path(@comment.spot)
+    else
+      flash[:notice] = "Nie można zostawić informacji bez treści lub autora"
+      redirect_to spot_path(@comment.spot)
+    end
   end
 end
--- app/controllers/spots_controller.rb
+++ app/controllers/spots_controller.rb
@@ -14,6 +14,7 @@ class SpotsController < ApplicationController
     @comments = @spot.comments
     @comment = Comment.new
     @comment.spot_id = @spot.id
+    @comment.save
   end
 
   # GET /spots/new

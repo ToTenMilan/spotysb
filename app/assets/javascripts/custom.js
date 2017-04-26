859f944eebdfa7a3bb4907a4f75ab45df1485b22
Grzegorz Milanowski <grzegorz.od.tych.milanowskich@gmail.com>
2017-04-26 22:01:56 +0200

    Cosmetic changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,11 +1,15 @@
 function howItWorks() {
-  alert("Wybierz spota na którego się wybierasz i zostaw notke z dowolną informacją, np z godziną o której będziesz.\n\nCyfra na zdjęciu oznacza ilość zostawionych notek pod nim.\n\nNotki są usuwanę każdego dnia o północy dlatego masz pewność że są aktualne.");
+  alert("Wybierz miejscówkę na którą się wybierasz i zostaw notke z dowolną informacją, np z godziną o której będziesz.\n\nCyfra na zdjęciu oznacza ilość zostawionych notek pod nim.\n\nNotki są usuwanę każdego dnia o północy dlatego masz pewność że są aktualne.");
 }
 
 function naDeske() {
-  alert("Ten link w przyszłości przekieruje cię na miejscówkę z największą ilością notek w danym dniu.");
+  alert("Ten link w przyszłości przekieruje cię na miejscówke z największą ilością notek w danym dniu.");
 }
 
 function beta() {
-  alert('To jest jeszcze w wersja beta, a nazwa "Spocik.pl" jeszcze nie jest "klepnięta" i może się zmienić.');
+  alert('To jest wersja beta, a nazwa "SpotySB.com" jeszcze może się zmienić.');
+}
+
+function addYourCity() {
+  alert('Ten link pozwoli Ci dodać nowe miasto i jego spoty')
 }
